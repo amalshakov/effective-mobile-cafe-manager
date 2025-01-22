7b80031006a9aee255bc855ae977aa2636f1c2b1
Alexander Malshakov <89681115653@mail.ru>
2025-01-22 11:54:19 +0300

    add OrderAdmin

--- cafe_manager/orders/admin.py
+++ cafe_manager/orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "table_number",
+        "total_price",
+        "status",
+        "created_at",
+    )
+    list_filter = ("status", "table_number")
+    search_fields = ("table_number",)
