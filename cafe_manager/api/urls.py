9f3f71ea587bd608b13ebbc8a9f96c23e91c7fa9
Alexander Malshakov <89681115653@mail.ru>
2025-01-22 14:47:00 +0300

    add urls for api

---
+++ cafe_manager/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import OrderViewSet
+
+
+app_name = "api"
+
+router = DefaultRouter()
+
+router.register(r"orders", OrderViewSet, basename="orders")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
